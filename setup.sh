5bd1e4b6d67c3db49bb6fd16c91d4eaa11b185bc
divusmetaphysica <divusmetaphysica@gmail.com>
2025-02-06 16:23:56 +0200

    Add node download for linux

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
+NODE_VERSION = '20.9.0'
 sudo apt-get install neovim
 sudo apt-get install fzf fd-find ripgrep gcc
+
+pushd /opt/
+sudo curl -o /opt/node-v$NODE_VERSION-linux-x64.tar https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz
+sudo ln -s /opt/node-v$NODE_VERSION-linux-x64/bin/node /usr/local/bin/node
+sudo ln -s /opt/node-v$NODE_VERSION-linux-x64/bin/npx /usr/local/bin/npx
+popd
 
 # https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/DejaVuSansMono.zip
 # https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.zip
